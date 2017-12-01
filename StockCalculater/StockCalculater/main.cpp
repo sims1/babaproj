3ff494bbef63640f80ed975cad35be1a4da19fc8
Ling He <hi.ling.he@gmail.com>
2017-11-30 21:41:39 -0500

    MEOW-199: 0 setting up IDE and version control on mac/windows

---
+++ StockCalculater/StockCalculater/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StockCalculater
+//
+//  Created by Ling on 11/30/17.
+//  Copyright © 2017 Ling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
